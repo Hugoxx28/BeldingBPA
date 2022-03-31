c0ab8ad1af5fc18c0555e5ae67768fdc7503b806
Hugoxx28 <97367995+Hugoxx28@users.noreply.github.com>
2022-03-31 14:28:31 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,352 @@
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".ideas__cluster-box1");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".ideas__cluster-box2");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".ideas__cluster-box3");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".ideas__cluster-box4");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible")
+  }
+})
+
+// Cluster scroll end animation 
+
+// Graphic scroll start animation 
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".percentage__img-graphic");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+
+
+// Graphic scroll end animation 
+
+// Percentage scroll start animation 
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".percentage-bar__bikes");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("animation-percentage")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".percentage-bar__scooters");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("animation-percentage2")
+  }
+})
+
+
+// Percentage scroll end animation 
+
+// Cards Personal scroll start animation 
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".personal-cards__bikes");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-bikes")
+  }
+})
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".personal-cards__bikes-container ");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-bikes-container")
+  }
+})
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".personal-cards__scooters");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-scooters")
+  }
+})
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".personal-cards__scooters-container");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-scooters-container")
+  }
+})
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".personal-map");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-map")
+  }
+})
+// Cards Personal scroll end animation 
+// About us scroll start animation 
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".about__members-hugo-img");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".about__members-hugo-text-container");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".about__members-evan-text-container");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".about__members-evan-img");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".about__members-brendan-text-container");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".about__members-brendan-img");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+// About us scroll end animation 
+// Recycling scroll start animation 
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".cycle-plan__results");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-pop")
+  }
+})
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".cycle-plan__plan-title");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-underline")
+  }
+})
+
+
+// Recycling scroll end animation 
+// Publict scroll start animation 
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".publict-cards__bus");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".publict-cards__taxi");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".publict-cards__gondola");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".slideshow-container");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+// Publict scroll end animation 
+// Energy scroll start animation 
+
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".energy-doing__hydro-img");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".energy-cards__solar");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".energy-cards__lamps");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".energy-cards__charger");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".energy-conclusion__img");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition){
+    content.classList.add("visible-graphic")
+  }
+})
+// Energy scroll end animation 
+
+
+
+let hamburguer = document.querySelector(".hamburguer-menu");
+
+
+window.addEventListener('scroll', function()  {
+    let hamburguer = document.querySelector(".hamburger-menu");
+    let posicion = hamburguer.getBoundingClientRect().top;
+     if (posicion < -80){
+       hamburguer.classList.add("hamburguer-menu-scroll")
+     } 
+})
+
+document = mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  console.log("ran");
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+var btn = $("#top-button-id");
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 150) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
